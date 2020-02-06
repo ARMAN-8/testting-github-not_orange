a3f5ffbee38a5f3ef10aaf69cc369bb935a34e29
Arman <galamat.aa@gmail.com>
2020-02-06 10:26:04 +0600

    New branch named "Desktop"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Khm Khm
                 </div>
 
                 <div class="links">
